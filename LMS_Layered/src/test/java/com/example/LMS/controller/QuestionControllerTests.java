533998beb9eb46aab945d10d042890bfafd2734d
andrew adel <68513548+android-dedo@users.noreply.github.com>
2024-12-23 11:57:20 +0200

    Update QuestionControllerTests.java

--- LMS_Layered/src/test/java/com/example/LMS/controller/QuestionControllerTests.java
+++ LMS_Layered/src/test/java/com/example/LMS/controller/QuestionControllerTests.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-class QuestionControllerTest {
+class QuestionControllerTests {
 
     @Mock
     private QuestionService questionService;
